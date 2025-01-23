6461304e3d4b9f8e33456f94bce542ff884201ba
Honest0 <jh4169306@gmail.com>
2025-01-23 02:30:15 +0100

    upload_background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,21 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter()
   return (
-    <div className="flex justify-center items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="background flex justify-center items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div
-        className="transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
+        className="text-2xl font-semibold opacity-75 transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
         onClick={() => router.push('/home')}
       >DÉTECTEUR DE PROJETS</div>
       <div
-        className="transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
+        className="text-2xl font-semibold opacity-75 transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
         // onClick={() => router.push('/home')}
       >VISUALISATION</div>
       <div
-        className="transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
+        className="text-2xl font-semibold opacity-75 transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
         // onClick={() => router.push('/home')}
       >SUIVI WALLET</div>
       <div
-        className="transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
+        className="text-2xl font-semibold opacity-75 transition-all cursor-pointer hover:opacity-80 p-20 border-[1px] border-zinc-600 hover:border-zinc-800 rounded-3xl bg-zinc-100 hover:bg-zinc-300"
         // onClick={() => router.push('/home')}
       >NOUVEAUX PROJETS</div>
     </div>
